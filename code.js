78a933517f7d6b7b78016c293b18f02a527cb42e
Sean W <104464288+swilso59@users.noreply.github.com>
2023-11-10 08:26:01 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
 function mergesort(array) {
+    // Get the length og the input array.
+    const n = array.length;
+    // Create variable for size of subarray 
+    var subSize; 
+    // Create variable to keep track of left subarray 
+    var leftStart;
+    // Start loop for subarrays 
+    for (subSize =  1; subSize < n; subSize = 
+    
+    
     return array;
 }
